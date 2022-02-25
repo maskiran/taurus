af0b4e3e5752df998301ebc9359e2fe014f463f4
Kiran Madabhushi <maskiran@gmail.com>
2022-02-25 12:54:45 -0800

    use start and end time from the first and the last test case files

--- report.py
+++ report.py
@@ -14,27 +14,34 @@ def test_case_to_json(o: TestCase):
 
 
 class Report:
-    def __init__(self, test_case_files: List[TestCaseFile], log_dir: str,
-                 start_time: datetime, end_time: datetime):
+    def __init__(self, test_case_files: List[TestCaseFile], log_dir: str):
         self.test_case_files: List[TestCaseFile] = test_case_files
         self.log_dir = log_dir
-        self.start_time: datetime.datetime = start_time
-        self.end_time: datetime.datetime = end_time
-        self.duration = (end_time - start_time).total_seconds()
+        self.start_time: datetime.datetime = None
+        self.end_time: datetime.datetime = None
+        self.duration:int = 0
         self.total: int = 0
         self.passed: int = 0
         self.failed: int = 0
+        self.skipped: int = 0
         self.failed_test_cases: List[TestCase] = []
         self.generate_stats()
         self.generate_json_report()
         self.generate_summary()
 
     def generate_stats(self):
+        # get the start and end time from the first and the last file
+        self.start_time = self.test_case_files[0].start_time
+        self.end_time = self.test_case_files[-1].end_time
+        self.duration = (self.end_time - self.start_time).total_seconds()
         for tc_file in self.test_case_files:
             for tc in tc_file.get_test_cases():
                 self.total += 1
-                if tc.status == 'passed':
+                if tc.status == "passed":
                     self.passed += 1
+                elif tc.status == "":
+                    self.skipped += 1
+                    self.failed_test_cases.append(tc)
                 else:
                     self.failed += 1
                     self.failed_test_cases.append(tc)
@@ -47,6 +54,7 @@ class Report:
                     'total': self.total,
                     'passed': self.passed,
                     'failed': self.failed,
+                    'skipped': self.skipped,
                     'start_time': str(self.start_time),
                     'end_time': str(self.end_time),
                     'duration': self.duration,
@@ -57,14 +65,14 @@ class Report:
             json.dump(data, fd, default=test_case_to_json, indent=4)
 
     def generate_summary(self):
-        data = f"Total: {self.total}, Passed: {self.passed}, Failed: {self.failed}\n"
+        data = f"Total: {self.total}, Passed: {self.passed}, Failed: {self.failed}, Skipped: {self.skipped}\n"
         data += f"Start Time: {self.start_time}, End Time: {self.end_time}\n"
         data += f"Duration: {self.duration} secs\n\n"
-        data += "Failed Test Cases:\n"
+        data += "Failed/Skipped Test Cases:\n"
         ftc_data = []
         for ftc in self.failed_test_cases:
-            ftc_data.append([ftc.full_name, ftc.error])
-        data += tabulate(ftc_data, tablefmt="grid")
+            ftc_data.append([ftc.full_name, ftc.error or "Skipped"])
+        data += tabulate(ftc_data, headers=['Test Case', 'Reason'], tablefmt="grid")
         summary_file = os.path.join(self.log_dir, 'summary.txt')
         with open(summary_file, 'w') as fd:
             fd.write(data)
